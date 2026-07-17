64290d9903292271f8d32397e6801a9bd9b03191
AhmedRamadan34 <108575426+AhmedRamadan34@users.noreply.github.com>
2026-07-17 16:19:12 +0300

    Add login system

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ import os
 import asyncio
 from video_generator import generate_video
 from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="my_super_secret_key"
+)
 
 app.mount(
     "/static",
@@ -24,7 +30,47 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 LAST_VIDEO = ""
 
 @app.get("/")
-async def home(request: Request):
+async def login_page(request: Request):
+
+    return templates.TemplateResponse(
+        request=request,
+        name="login.html"
+    )
+
+@app.post("/login")
+async def login(
+    request: Request,
+    username: str = Form(...),
+    password: str = Form(...)
+):
+
+    if username == "admin" and password == "123456":
+
+        request.session["user"] = username
+
+        return RedirectResponse(
+            "/studio",
+            status_code=303
+        )
+
+    return templates.TemplateResponse(
+        request=request,
+        name="login.html",
+        context={
+            "request": request,
+            "error": "Invalid Username or Password"
+        }
+    )
+
+@app.get("/studio")
+async def studio(request: Request):
+
+    if "user" not in request.session:
+
+        return RedirectResponse(
+            "/",
+            status_code=303
+        )
 
     return templates.TemplateResponse(
         request=request,
